19ba4c459c7ffa48e1deb893a5a351234359e0b7
wighthood <zefires@gmail.com>
2023-11-22 16:26:50 +0100

    bit of cleaning

--- Gorilla/Assets/Move.cs
+++ Gorilla/Assets/Move.cs
@@ -2,25 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MouvementPersonnage : MonoBehaviour
+public class Move : MonoBehaviour
 {
+    // Update is called once per frame
     public float vitesse = 5f;
-    public float forceGravite = 9.8f; // Ajustez selon votre gravité
-    private Rigidbody2D rb;
-
-    void Start()
-    {
-        rb = GetComponent<Rigidbody2D>();
-    }
 
     void Update()
     {
+        // Récupérer les entrées du joueur
         float deplacementHorizontal = Input.GetAxis("Horizontal");
-        Vector2 deplacement = new Vector2(deplacementHorizontal * vitesse, rb.velocity.y);
-        rb.velocity = deplacement;
-
-        // Appliquer la gravité manuellement
-        Vector2 gravite = new Vector2(0f, -forceGravite * rb.mass);
-        rb.AddForce(gravite);
+ 
+        // Déplacer le personnage en utilisant le Rigidbody2D
+        GetComponent<Rigidbody2D>().velocity = new Vector2(deplacementHorizontal * vitesse, -1f);
     }
 }
