32f36234ce444de4bdff1cb41804d7142b0028a2
wighthood <zefires@gmail.com>
2023-12-01 15:50:22 +0100

    aiming AI

--- Gorilla/Assets/_Scripts/A.IM.cs
+++ Gorilla/Assets/_Scripts/A.IM.cs
@@ -7,6 +7,7 @@ using UnityEngine.InputSystem;
 
 public class A_IM : MonoBehaviour
 {
+    public GameObject player;
     [SerializeField] GameObject Bullet;
     public float angle;
     public int dir;
@@ -20,21 +21,89 @@ public class A_IM : MonoBehaviour
     IEnumerator delay()
     {
         yield return new WaitForSeconds(Delay);
-        Throwball_AI();
+        Search();
+    }
+    private void Search()
+    {
+        bool detect = false;
+        dir = System.Math.Sign(player.GetComponent<Rigidbody2D>().position.x - transform.position.x);
+        for (int i = 1; i < 10; i++)
+        {
+            launchForce = i * 2;
+            float angle_max = 180 * dir;
+            float angle_min = 45 * dir;
+            for (int j = 0; j < 20; j++)
+            {
+                float mid = (angle_max + angle_min) / 2;
+                float x = Mathf.Cos((mid - 90) * dir * Mathf.PI / 180);
+                float y = Mathf.Sin((mid - 90) * Mathf.PI / 180);
+                Vector2 velocity = new Vector2(x * launchForce, y * launchForce);
+                Vector2 curpos = new Vector2(transform.position.x + x, transform.position.y + y);
+                Vector2 lastpos = curpos;
+                Vector2 gravity = new Vector2(0, -9.80665f);
+                while (!detect)
+                {
+                    curpos += velocity * Time.fixedDeltaTime;
+                    velocity += gravity * Time.fixedDeltaTime;
+                    Debug.DrawLine(lastpos, curpos);
+                    var raycast = Physics2D.CircleCast(curpos, 0.25f, Vector2.zero);
+                    if (raycast.collider)
+                    {
+                        if (raycast.collider != null)
+                        {
+                            if (raycast.collider.transform.tag == "Map")
+                            {
+                                break;
+                            }
+                            else if (raycast.collider.transform.tag == "Player")
+                            {
+                                detect = true;
+                                Throwball_AI (new Vector2(transform.position.x + x, transform.position.y + y), new Vector2(x * launchForce, y * launchForce));
+                            }
+                        }
+                    }
+                    lastpos = curpos;
+                }
+                if (dir == 1)
+                {
+                    if (player.GetComponent<Rigidbody2D>().position.x > curpos.x)
+                    {
+                        angle_min = mid;
+                    }
+                    else
+                    {
+                        angle_max = mid;
+                    }
+                }
+                else if (dir == -1)
+                {
+                    if (player.GetComponent<Rigidbody2D>().position.x < curpos.x)
+                    {
+                        angle_min = mid;
+                    }
+                    else
+                    {
+                        angle_max = mid;
+                    }
+                }
+
+                if (detect)
+                {
+                    break;
+                }
+            }
+            if (detect)
+            {
+                break;
+            }
+        }
+        StartCoroutine(delay());
     }
 
-
-    public void Throwball_AI()
+    public void Throwball_AI(Vector2 spawn, Vector2 direction)
     {
-        angle = Random.Range(45, 180)*dir;
-        launchForce = Random.Range(1, 20);
-        float x = Mathf.Cos((angle - 90) * dir * Mathf.PI / 180);
-        float y = Mathf.Sin((angle - 90) * Mathf.PI / 180);
-        Vector3 direction = new Vector3(x * launchForce, y * launchForce, 0);
-        Vector3 spawn = new Vector3(transform.position.x + x, transform.position.y + y, 0);
         GameObject proj = Instantiate(Bullet, spawn, transform.rotation);
         proj.GetComponent<Bullet_Movement>().SetBullet((Vector3)direction);
-        StartCoroutine(delay());
     }
    
 }
